b5e7fe86db411730e2970f03ef7b3e8f248c6349
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2025-03-22 19:13:09 +0100

    Tailwind reinstalled + visual improvements

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        surface: '#12131C',
+        primary: '#00D5FF',
+        accent: '#FF5C38',
+        secondary: '#B47FFF',
+        highlight: '#FFCC33',
+        soft: '#D3D9E2',
+      },
+      fontFamily: {
+        primary: ['Unbounded', 'sans-serif'],
+        secondary: ['Public Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
